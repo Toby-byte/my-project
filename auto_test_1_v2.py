b2ac4a86cc9d5f01508b12d43307aa81acc71688
Toby-byte <tobiasbogenielsen@gmail.com>
2024-04-18 13:44:07 +0200

    test of subprocess

--- auto_test_1_v2.py
+++ auto_test_1_v2.py
@@ -1,13 +1,13 @@
 import subprocess
 
 # Activate the virtual environment
-subprocess.run(".\\Scripts\\activate", shell=True)
+subprocess.run(".\\Scripts\\activate", shell=True) # does not work with subprocess
 
 # Export the list of installed packages to requirements.txt
-subprocess.run("pip freeze > requirements.txt", shell=True)
+subprocess.run("pip freeze > requirements.txt", shell=True) # works
 
 # Install packages from requirements.txt
-subprocess.run("pip install -r requirements.txt", shell=True)
+subprocess.run("pip install -r requirements.txt", shell=True) # works
 
 # Deactivate the virtual environment
-subprocess.run("deactivate", shell=True)
+subprocess.run("deactivate", shell=True) # does not work with subprocess
